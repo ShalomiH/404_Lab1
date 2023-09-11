6654e17c2169a91d75660197e0b202f38bd20497
ShalomiH <90998772+ShalomiH@users.noreply.github.com>
2023-09-11 17:45:01 -0600

    Updated script

--- test1.py
+++ test1.py
@@ -1,3 +1,9 @@
 import requests
 
 print(requests.__version__)
+
+url = "https://raw.githubusercontent.com/ShalomiH/404_Lab1/main/test1.py"
+print(requests.get("https://www.google.com/"))
+
+res = requests.get(url)
+print(res.text)
