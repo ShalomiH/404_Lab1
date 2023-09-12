92b85ac2266d78040be63df47a83d497b26c0afa
ShalomiH <90998772+ShalomiH@users.noreply.github.com>
2023-09-11 18:15:31 -0600

    Updated google URL

--- test1.py
+++ test1.py
@@ -3,7 +3,7 @@ import requests
 print(requests.__version__)
 
 url = "https://raw.githubusercontent.com/ShalomiH/404_Lab1/main/test1.py"
-print(requests.get("https://www.google.com/"))
+print(requests.get("http://google.com/"))
 
 res = requests.get(url)
 print(res.text)
